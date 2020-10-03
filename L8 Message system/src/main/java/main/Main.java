ba9a5dbea4a5e66698c2405bfb75c24b9faf4cf6
3ANov <3anovdev@gmail.com>
2020-10-03 10:52:10 +0300

    Решение для задачи 8

--- L8 Message system/src/main/java/main/Main.java
+++ L8 Message system/src/main/java/main/Main.java
@@ -2,7 +2,9 @@ package main;
 
 
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.logging.Logger;
 
 
@@ -12,8 +14,38 @@ public class Main {
     public static void main(String[] args) throws Exception {
         log.info("Server start");
 
+        try(ServerSocketChannel serverSocketChannel = ServerSocketChannel.open()) {
+
+            serverSocketChannel.socket().bind(new InetSocketAddress(5050));
+            serverSocketChannel.configureBlocking(false);
+
+            while (true) {
+                SocketChannel socketChannel =
+                        serverSocketChannel.accept();
+                log.info("Server started");
+                if (socketChannel != null) {
+                    ByteBuffer buf = ByteBuffer.allocate(48);
+                    int bytesRead = socketChannel.read(buf);
+
+                    while (bytesRead != -1) {
+
+                        buf.flip();  //make buffer ready for read
+
+                        while (buf.hasRemaining()) {
+                            System.out.print((char) buf.get()); // read 1 byte at a time
+                        }
+
+                        buf.clear(); //make buffer ready for writing
+                        bytesRead = socketChannel.read(buf);
+                    }
+                }
+            }
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
 
-        
+
     }
 }
