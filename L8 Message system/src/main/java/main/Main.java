bc843f957717a46d764550734d8886ee13c11180
3ANov <3anovdev@gmail.com>
2020-10-03 12:11:40 +0300

    Решение для задачи 8

--- L8 Message system/src/main/java/main/Main.java
+++ L8 Message system/src/main/java/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
 
+import sockets.Server;
+
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
@@ -12,39 +14,9 @@ public class Main {
     private static Logger log = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) throws Exception {
-        log.info("Server start");
-
-        try(ServerSocketChannel serverSocketChannel = ServerSocketChannel.open()) {
-
-            serverSocketChannel.socket().bind(new InetSocketAddress(5050));
-            serverSocketChannel.configureBlocking(false);
-
-            while (true) {
-                SocketChannel socketChannel =
-                        serverSocketChannel.accept();
-                log.info("Server started");
-                if (socketChannel != null) {
-                    ByteBuffer buf = ByteBuffer.allocate(48);
-                    int bytesRead = socketChannel.read(buf);
-
-                    while (bytesRead != -1) {
-
-                        buf.flip();  //make buffer ready for read
-
-                        while (buf.hasRemaining()) {
-                            System.out.print((char) buf.get()); // read 1 byte at a time
-                        }
-
-                        buf.clear(); //make buffer ready for writing
-                        bytesRead = socketChannel.read(buf);
-                    }
-                }
-            }
-        }
-        catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-
+        Server server = new Server(5050, 10);
+        log.info("Server started");
+        server.start();
 
 
     }
--- L8 Message system/src/main/java/sockets/Server.java
+++ L8 Message system/src/main/java/sockets/Server.java
@@ -1,23 +1,39 @@
 package sockets;
 
+import main.Main;
+
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.channels.ServerSocketChannel;
-import java.util.concurrent.Executor;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Logger;
 
-public class Server{
+public class Server {
+    private static Logger log = Logger.getLogger(Server.class.getName());
+
     ExecutorService pool;
-    private final ServerSocketChannel serverSocketChannel;
+    private final int port;
+    private final int threadCount;
 
-    public Server(int port, int threadCount) throws IOException {
-        this.serverSocketChannel = ServerSocketChannel.open();
-        serverSocketChannel.socket().bind(new InetSocketAddress(port));
+
+    public Server(int port, int threadCount) {
+        this.port = port;
         this.pool = Executors.newFixedThreadPool(threadCount);
+        this.threadCount = threadCount;
     }
 
+    public void start() {
+        try (ServerSocket server = new ServerSocket(port)) {
+            while (!server.isClosed()) {
+                Socket client = server.accept();
+                pool.execute(new ClientsSocketThread(client));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
-
+    }
 }
